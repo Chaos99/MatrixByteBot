ea2d6f0851e3a1ae1db9a501270bed7fb1480de9
Chaos99 <stefan@binaervarianz.de>
2018-10-23 12:21:40 +0200

    added new keyword and diversified first_run

--- maintenanceplugin.py
+++ maintenanceplugin.py
@@ -22,9 +22,10 @@ class MaintenancePlugin(Plugin):
         Plugin.add_matcher(self, re.compile("![Vv]ersion"))
         Plugin.add_matcher(self, re.compile("![Ll]ist"))
         Plugin.add_matcher(self, re.compile("![Uu]ptime"))
+        Plugin.add_matcher(self, re.compile("![Hh]istory"))
 
         self.bot = bot #safe for later use
-        self.first_run = True
+        self.first_run = {plugins:True, version:True, history:True}
         self.plugin_names = ""
 
         self.version = "no version set"
@@ -33,17 +34,36 @@ class MaintenancePlugin(Plugin):
 
     def collect_plugins(self):
         """traverse plugin list in bot an get names"""
-        if not self.first_run:
+        if not self.first_run['plugins']:
             pass
         else:
             for plugin in self.bot.plugins:
                 self.plugin_names += (plugin.name + '\n')
-            self.first_run = False
+            self.first_run['plugins'] = False
         return self.plugin_names
 
     def get_version(self):
         """ get version number from topmost entry in CHANGELOG.mf"""
-        if not self.first_run:
+        if not self.first_run['version']:
+            pass
+        else:            
+            pattern = re.compile(r'##\s*(.*)')
+            for line in open('CHANGELOG.md'):
+                match = re.match(pattern, line)
+                if match is not None:
+                    self.version = match.group(1)
+                    break #stop at first match
+            self.first_run['version'] = False
+        return self.version
+
+    def get_uptime(self):
+        """ calculate uotime since start"""
+        uptime = datetime.datetime.now() - self.start_time
+        return str(uptime)
+    
+    def get_history(self):
+        """ extract a few lines from the CHANGELOG file"""
+        if not self.first_run['history']:
             pass
         else:
             pattern = re.compile(r'##\s*(.*)')
@@ -52,12 +72,8 @@ class MaintenancePlugin(Plugin):
                 if match is not None:
                     self.version = match.group(1)
                     break #stop at first match
+            self.first_run['history'] = False
         return self.version
-
-    def get_uptime(self):
-        """ calculate uotime since start"""
-        uptime = datetime.datetime.now() - self.start_time
-        return str(uptime)
 
     def callback(self, room, event):
         """ collect info based on input match"""
@@ -68,6 +84,8 @@ class MaintenancePlugin(Plugin):
             room.send_text(self.collect_plugins())
         if re.compile("![Uu]ptime").match(event['content']['body']):
             room.send_text(self.get_uptime())
+        if re.compile("!Hi]istory").match(event['content']['body']):
+            room.send_text(self.get_history())
 
     def get_help(self):
         """Return help text"""

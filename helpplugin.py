bfdefd734c0bd99cf3df21ad523fbd3eb8c8c4c2
Chaos99 <stefan@binaervarianz.de>
2018-10-23 10:15:34 +0200

    add about text to helpplugin

--- helpplugin.py
+++ helpplugin.py
@@ -20,6 +20,7 @@ class HelpPlugin(Plugin):
         Plugin.__init__(self, name, bot)
         HELP_LOG.debug("Adding matcher for '!Help'")
         Plugin.add_matcher(self, re.compile("![Hh]elp"))
+        Plugin.add_matcher(self, re.compile("![Aa]bout"))
 
         self.bot = bot #safe for later use
         self.help_text = ""
@@ -41,11 +42,23 @@ class HelpPlugin(Plugin):
 
         return self.help_text
 
+    def get_about(self):
+        text = ("MatrixByteBot by @Chaos:erfurt.chat\n"
+                "GNU General Public License v3.0 (https://www.gnu.org/licenses/gpl.html)\n"
+                "Sources at https://github.com/Chaos99/MatrixByteBot\n"
+                "This is an alpha version with just partial functionality and a lot of bugs!\n"
+                "\n Please use Github to submit bug reports or feature requests.")
+        return text        
+        
     def callback(self, room, event):
         """send collected help messages"""
         HELP_LOG.debug("%s sends response", self.name)
-        room.send_text(self.collect_help())
+        if re.compile("![Aa]bout").match(event['content']['body']):
+            room.send_text(self.get_about())
+        else:
+            room.send_text(self.collect_help())
 
     def get_help(self):
         """Return help text"""
-        return "Prints this help text on !help"
+        return ("Prints this help text on !help\n"
+                "and some meta info on !about")
